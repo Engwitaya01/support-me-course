d3891843e036fce199490705665aa10c3414b847
Witaya252525 <witaya.c@kkumail.com>
2026-04-12 21:40:14 +0700

    feat: update homepage layout and add responsive typography

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,18 +1,25 @@
-
-
-
 import { Button } from "@/components/ui/button";
+import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
 //rfc homepage
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold mb-4">Welcome to the Homepage</h1>
-      <p className="text-lg text-gray-600">This is the homepage of our application.</p>
-        <Button className="mt-6"> Login </Button>  <Button className="mt-6"> Sign Up </Button>
+    <>
+      <h1 className="flex gap-2 items-center ">
+        <PersonStandingIcon size={50} className=" text-pink-500" /> Support
+        Me{" "}
+      </h1>
+      <p>This is the dashboard of our application.</p>
+      <div className="mt-6 flex items-center gap-3">
+        <Button asChild>
+          <Link href="/login">Login</Link>
+        </Button>
 
-    </div>
+        <small>or</small>
+        <Button variant="outline" asChild>
+          <Link href="/sign-up">Sign Up</Link>
+        </Button>
+      </div>
+    </>
   );
 }
-
-
-
